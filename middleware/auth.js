20661073b83c2e09228dd1dfbdf9c6bc8706729e
Ashar Shahid <ashar2shahid@gmail.com>
2019-05-25 22:24:41 +0530

    Setting up authentication middleware for get request

---
+++ middleware/auth.js
@@ -0,0 +1,44 @@
+const crypto = require("crypto");
+const secret_config = require("../config/server-config.json");
+
+module.exports = function(req, res, next) {
+  //Get token from header
+  const token = req.header("x-auth-token");
+
+  //Check if no token
+  if (!token) {
+    return res.status(401).json({ msg: "No token, authorization denied" });
+  }
+
+  //Verify token
+  try {
+    // create a aes267 decipher based on our key
+    const decipher = crypto.createDecipheriv("aes-256-cbc", secret_config.secretKey, secret_config.iv);
+    // update the decipher with our encrypted string
+    let decrypted_token = decipher.update(token, "base64", "utf8");
+
+    decrypted_token += decipher.final("utf8");
+
+    decrypted_object = JSON.parse(decrypted_token);
+
+    const server_hash = crypto
+      .createHash("md5")
+      .update(secret_config.ServerSecret)
+      .digest("hex");
+    const authenitcation_attempt_hash = decrypted_object.serverHash;
+
+    if (server_hash !== authenitcation_attempt_hash) throw "Invalid Token hash";
+
+    const token_expire_date = decrypted_object.created_at + decrypted_object.expire_time;
+
+    console.log(token_expire_date, Math.round(new Date() / 1000));
+
+    if (token_expire_date < Math.round(new Date() / 1000)) throw "Invalid Token time";
+
+    req.user = decrypted_object.id;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(401).json({ msg: "Token is not valid" });
+  }
+};
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const validateUserData = require("../../validation/users");
+const crypto = require("crypto");
+const secret_config = require("../../config/server-config.json");
 const connectDB = require("../../config/db");
+const auth = require("../../middleware/auth");
+
+// Query to create table if it doesn't exist
+const create_usertable_query = `CREATE TABLE IF NOT EXISTS customers (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL, 
+    last_name VARCHAR(255) NOT NULL, 
+    father_name VARCHAR(255) NOT NULL, 
+    pan_number VARCHAR(255) NOT NULL, 
+    date_of_birth DATE NOT NULL,
+    gender ENUM('male','female') NOT NULL, 
+    email VARCHAR(255) NOT NULL, 
+    address VARCHAR(255) NOT NULL, 
+    profile_image VARCHAR(255) NOT NULL,
+    UNIQUE(pan_number) 
+  )`;
 
 // @route POST api/users
 // @desc POST User details
@@ -11,29 +29,12 @@ router.post("/", async (req, res) => {
   try {
     // Validate Input Data
     const { errors, isValid } = validateUserData(req.body);
-
     // Check Validation
     if (!isValid) {
       return res.status(400).json({ errors: errors });
     }
-
     // Check if table exists otherwise create it
-    const result_table_creation = await connectDB.connection_mysql.query(
-      `CREATE TABLE IF NOT EXISTS customers (
-        id INT AUTO_INCREMENT PRIMARY KEY,
-        first_name VARCHAR(255) NOT NULL, 
-        last_name VARCHAR(255) NOT NULL, 
-        father_name VARCHAR(255) NOT NULL, 
-        pan_number VARCHAR(255) NOT NULL, 
-        date_of_birth DATE NOT NULL,
-        gender ENUM('male','female') NOT NULL, 
-        email VARCHAR(255) NOT NULL, 
-        address VARCHAR(255) NOT NULL, 
-        profile_image VARCHAR(255) NOT NULL,
-        UNIQUE(pan_number) 
-      )`
-    );
-
+    const result_table_creation = await connectDB.connection_mysql.query(create_usertable_query);
     let record = [
       [
         req.body.first_name,
@@ -47,18 +48,33 @@ router.post("/", async (req, res) => {
         req.body.profile_image
       ]
     ];
-
     // Insert into table
     const result_insertion_table = await connectDB.connection_mysql.query(
-      "INSERT INTO customers (first_name,last_name,father_name,pan_number,date_of_birth,gender,email,address,profile_image) VALUES ?",
+      `INSERT INTO customers (first_name,last_name,father_name,pan_number,date_of_birth,gender,email,address,profile_image) VALUES ? ;`,
       [record]
     );
 
-    return res.json({ msg: result_insertion_table });
+    // Unique token signature for each token
+    let signature = JSON.stringify({
+      id: result_insertion_table[0].insertId,
+      created_at: Math.round(new Date() / 1000),
+      expire_time: 300,
+      serverHash: crypto
+        .createHash("md5")
+        .update(secret_config.ServerSecret)
+        .digest("hex")
+    });
+
+    const cipher = crypto.createCipheriv("aes-256-cbc", secret_config.secretKey, secret_config.iv);
+    let token = cipher.update(signature, "utf8", "base64");
+    token += cipher.final("base64");
+
+    return res.json({ token: token });
   } catch (err) {
     console.log(err);
     let print_error = "Server Error";
-    if (err.code === "ER_DUP_ENTRY") print_error = "Pan Number has already been added - Server Error";
+    if (err.code === "ER_DUP_ENTRY") print_error = "Pan Number has already been added";
+
     res.status(500).send(print_error);
   }
 });
@@ -67,6 +83,15 @@ router.post("/", async (req, res) => {
 // @desc GET User details
 // @access Private
 
-router.get("/", (req, res) => res.send("User route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const [result, fields] = await connectDB.connection_mysql.query("SELECT * FROM customers");
+
+    res.json(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send(err);
+  }
+});
 
 module.exports = router;
